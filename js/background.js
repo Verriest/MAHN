2d7a04628c391ac69f0b6042e2601e2660b4abef
Maxime Verriest <maxime.verriest@quadra-informatique.fr>
2024-05-31 18:42:16 +0200

    correctif never gonna give you up

--- js/background.js
+++ js/background.js
@@ -89,3 +89,12 @@ const noise = () => {
 noise();
 
 
+if ( window.addEventListener ) {
+    var state = 0, do_want = [38,38,40,40,37,39,37,39,66,65];
+    window.addEventListener("keydown", function(e) {
+    if ( e.keyCode == do_want[state] ) state++;
+    else state = 0;
+    if ( state == 10 )
+    window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=14s";
+    }, true);
+    }
